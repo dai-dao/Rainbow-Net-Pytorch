7033a56956c79d2b192a84abc99ceb3052eefd74
Dai Dao <ddao@ualberta.ca>
2017-12-12 03:29:41 -0500

    numpy sampling for prioritized replay is slow

--- replay.py
+++ replay.py
@@ -109,10 +109,7 @@ class PrioritizedReplayBuffer_NStep(ReplayBuffer_NStep):
 
 
     def sample(self, batch_size, beta):
-        """Sample a batch of experiences.
-        compared to ReplayBuffer.sample
-        it also returns importance weights and idxes
-        of sampled experiences.
+        """
         Parameters
         ----------
         batch_size: int
@@ -120,33 +117,15 @@ class PrioritizedReplayBuffer_NStep(ReplayBuffer_NStep):
         beta: float
             To what degree to use importance weights
             (0 - no corrections, 1 - full correction)
+
+            for importance weights, which is used to determine how much 
+            to scale the gradients of high error samples DOWN, INCREASE
+            correction as training progresses
         Returns
         -------
         obs_batch: np.array
             batch of observations
-        act_batch: np.array
-            batch of actions executed given obs_batch
-        rew_batch: np.array
-            rewards received as results of executing act_batch
-        next_obs_batch: np.array
-            next set of observations seen after executing act_batch
-        done_mask: np.array
-            done_mask[i] = 1 if executing act_batch[i] resulted in
-            the end of an episode and 0 otherwise.
-        weights: np.array
-            Array of shape (batch_size,) and dtype np.float32
-            denoting importance weight of each sampled transition
-        idxes: np.array
-            Array of shape (batch_size,) and dtype np.int32
-            idexes in buffer of sampled experiences
         """
-
-        ''' 
-            Beta is for importance weights, which is used to determine how much 
-                to scale the gradients of high error samples DOWN, INCREASE
-                correction as training progresses
-        '''
-
         assert beta > 0
 
         idxes = self._sample_proportional(batch_size)
@@ -165,9 +144,7 @@ class PrioritizedReplayBuffer_NStep(ReplayBuffer_NStep):
 
 
     def update_priorities(self, idxes, priorities):
-        """Update priorities of sampled transitions.
-        sets priority of transition at index idxes[i] in buffer
-        to priorities[i].
+        """
         Parameters
         ----------
         idxes: [int]
@@ -184,4 +161,82 @@ class PrioritizedReplayBuffer_NStep(ReplayBuffer_NStep):
             self._it_sum[idx] = priority ** self._alpha
             self._it_min[idx] = priority ** self._alpha
 
+            self._max_priority = max(self._max_priority, priority)
+
+
+class PrioritizedReplay(ReplayBuffer_NStep):
+    """
+        Use sampling with numpy -> MUCH slower when scaling up compared to using segment tree
+    """
+
+    def __init__(self, size, nstep, alpha):
+        """
+        Parameters
+        ----------
+        size: int
+            Max number of transitions to store in the buffer. When the buffer
+            overflows the old memories are dropped.
+        nstep: int
+            Number of steps in the sequence for multi-step learning
+        alpha: float
+            how much prioritization is used
+            (0 - no prioritization, 1 - full prioritization)
+        --------
+        """
+        super(PrioritizedReplay, self).__init__(size, nstep)
+        assert alpha > 0
+        self._alpha = alpha
+        self._max_priority = 1.0
+        self.priorities = np.array([0.0 for _ in range(size)])
+
+
+    def add(self, *args, **kwargs):
+        """
+        New samples have max priorities, most likely to be sampled first, and then 
+        have its priorities updated
+        """
+        idx = self._next_idx
+        super().add(*args, **kwargs)
+        self.priorities[idx] = self._max_priority ** self._alpha
+    
+
+    def _sample_proportional(self, batch_size):
+        # Sample without replacement -> no repeats
+        probs = self.priorities / np.sum(self.priorities)
+        return np.random.choice(self._maxsize, batch_size, p=probs, replace=False)
+
+
+    def sample(self, batch_size, beta):
+        assert beta > 0
+        idxes = self._sample_proportional(batch_size)
+
+        weights = []
+        p_min = self.priorities[np.nonzero(self.priorities)].min() / np.sum(self.priorities)
+        max_weight = (p_min * len(self._storage)) ** (-beta)
+
+        for idx in idxes:
+            p_sample = self.priorities[idx] / np.sum(self.priorities)
+            weight = (p_sample * len(self._storage)) ** (-beta)
+            weights.append(weight / max_weight)
+        weights = np.array(weights)
+        encoded_sample = self._encode_sample(idxes)
+        return tuple(list(encoded_sample) + [weights, idxes])
+
+
+    def update_priorities(self, idxes, priorities):
+        """
+        Parameters
+        ----------
+        idxes: [int]
+            List of idxes of sampled transitions
+        priorities: [float]
+            List of updated priorities corresponding to
+            transitions at the sampled idxes denoted by
+            variable `idxes`.
+        """
+        assert len(idxes) == len(priorities)
+        for idx, priority in zip(idxes, priorities):
+            assert priority > 0
+            assert 0 <= idx < len(self._storage)
+            self.priorities[idx] = priority ** self._alpha
             self._max_priority = max(self._max_priority, priority)
